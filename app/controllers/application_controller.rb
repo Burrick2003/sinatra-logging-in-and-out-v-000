0057839d6ccac4d526aa27dcb5dff6d3af5872ca
Blakeley Bond <burrick2003@yahoo.com>
2018-03-08 02:42:22 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-    @user = User.find_by(username: params["name"], password: params["password"])
+    @user = User.find_by(username: params["username"])
     session[:user_id] = @user.id #so confusing, is it user username user_id gonna redo
     redirect '/account'
   end
