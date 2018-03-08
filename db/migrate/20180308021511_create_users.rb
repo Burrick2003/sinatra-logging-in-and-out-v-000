2a9660e2ead664b0175746ae5235efbb7884961b
Blakeley Bond <burrick2003@yahoo.com>
2018-03-08 02:48:08 +0000

    automatically backed up by learn

--- db/migrate/20180308021511_create_users.rb
+++ db/migrate/20180308021511_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t| #from dog example in activerecord setup in sinatra
-      t.string :name
+      t.string :username
       t.string :password
       t.decimal :balance #http://edgeguides.rubyonrails.org/active_record_migrations.html for this
     end
